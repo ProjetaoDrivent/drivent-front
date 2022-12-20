ceb0e29a351bc7776359c63c68659f1d5bd2b1d9
jonas <pedrojonas.pj77@gmail.com>
2022-12-20 15:06:52 -0300

    feat: payment/ formcard

--- src/pages/Dashboard/Payment/paymentCard.js
+++ src/pages/Dashboard/Payment/paymentCard.js
@@ -1,4 +1,50 @@
+import React, { useState } from 'react';
+import Cards from 'react-credit-cards';
 
 export default function PaymentCard() {
-    
+  const [number, setNumber] = useState('');
+  const [name, setName] = useState('');
+  const [expiry, setExpiry] = useState('');
+  const [cvc, setCvc] = useState('');
+  const [focus, setFocus] = useState('');
+
+  return (
+    <div>
+      <form>
+        <Cards/>
+        <imput 
+          type='tel'
+          name='number'
+          placeholdeer='Card Number'
+          value={number}
+          onChange={e => setNumber(e.target.value)}
+          onFocus={e => setFocus(e.target.name)}
+        />
+        <imput 
+          type='text'
+          name='name'
+          placeholdeer='Name'
+          value={name}
+          onChange={e => setName(e.target.value)}
+          onFocus={e => setFocus(e.target.name)}
+        />
+        <imput 
+          type='text'
+          name='expiry'
+          placeholdeer='MM/YY Expiry'
+          value={expiry}
+          onChange={e => setExpiry(e.target.value)}
+          onFocus={e => setFocus(e.target.name)}
+        />
+        <imput 
+          type='tel'
+          name='cvc'
+          placeholdeer='CVC'
+          value={cvc}
+          onChange={e => setCvc(e.target.value)}
+          onFocus={e => setFocus(e.target.name)}
+        />
+      </form>
+    </div>
+  );
 }
