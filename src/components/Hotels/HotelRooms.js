908119184cf114b33156383035a59bf47565a433
aldusd <aldusdanielfsm@gmail.com>
2023-01-05 18:20:58 -0300

    feat: completes room cards layout

--- src/components/Hotels/HotelRooms.js
+++ src/components/Hotels/HotelRooms.js
@@ -5,6 +5,7 @@ import RoomCard from './RoomCard';
 
 export default function HotelRooms({ id }) {
   const [roomData, setRoomData] = useState();
+  const [selectedRoom, setSelectedRoom] = useState(0);
   const { getRooms } = useHotel.useRooms(id);
 
   useEffect(async() => {
@@ -18,8 +19,12 @@ export default function HotelRooms({ id }) {
       {roomData?.map(room => 
         <RoomCard 
           key={room.id}
+          id={room.id}
           name={room.name}
-          capacity={room.capacity} />
+          capacity={room.capacity}
+          occupation={1}
+          selectedRoom={selectedRoom}
+          setSelectedRoom={setSelectedRoom} />
       )
 
       }
--- src/components/Hotels/RoomCard.js
+++ src/components/Hotels/RoomCard.js
@@ -1,23 +1,40 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import CapacityIcons from './CapacityIcons';
 
-export default function RoomCard({ name, capacity }) {
+export default function RoomCard({ id, name, capacity, occupation, selectedRoom, setSelectedRoom }) {
+  const focusRoom = () => {
+    if(capacity === occupation) return;
+    setSelectedRoom(id);
+  };
+  
   return (
-    <Card>
+    <Card 
+      onClick={() => focusRoom()} 
+      isSelected={id === selectedRoom}
+      isFull={capacity === occupation} >
       <p>{name}</p>
+      <CapacityIcons 
+        capacity={capacity}
+        occupation={occupation}
+        isSelected={id === selectedRoom}
+        isFull={capacity === occupation} />
     </Card>
   );
 };
 
 const Card = styled.div`
   display: flex;
+  justify-content: space-between;
   border: solid 1px #e5e5e5;
   border-radius: 7px;
   width: 10rem;
-  margin-right: 0.8rem;
-  padding: 0.7vh 0.4rem;
-
+  margin: 0.5vh 0.8rem 0.5vh 0;
+  padding: 1vh 0.4rem;
+  background-color: ${props => props.isSelected ? '#FFEED2' : props.isFull ? '#CECECE' : 'white' };
+  
   p {
     font-size: 1.2rem;
-    /* font-weight: bold; */
+    color: ${props => props.isFull ? '#9D9D9D' : 'black'};
   }
 `;
