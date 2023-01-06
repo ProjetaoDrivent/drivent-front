dd912dc315fd0c870a9b6a7ecf5e401ac4711adb
Tissyane <tissyane@gmail.com>
2023-01-06 10:52:51 -0300

    feat: activities - showing warning message for unpaid and remote tickets

---
+++ src/components/Activities/index.js
@@ -0,0 +1,7 @@
+export default function Activities() {
+  return (
+    <>
+      <h4>Activities pretty soon</h4>
+    </>
+  );
+}
--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -2,17 +2,27 @@ import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
 import Warning from '../../../components/WarningMessage';
 import * as useTickets from '../../../hooks/api/useTickets';
+import Activities from '../../../components/Activities';
 
-export default function Activities() {
+export default function ActivitiesPage() {
   const { tickets } = useTickets.useTickets();
+  
+  function IncludesActivities() {
+    if (tickets?.TicketType.isRemote === false) {
+      return (<Activities/>);
+    } else {
+      return (<Warning>Sua modalidade de ingresso não necessita escolher
+        atividade. Você terá acesso a todas as atividades.</Warning>);
+    }
+  };
 
   return (
     <>
       <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
       {tickets?.status !== 'PAID' ? (
         <Warning>Você precisa ter confirmado pagamento antes de fazer a escolha de atividades</Warning>
-      ) : (
-        <h1>Activities soon</h1>
+      ) : (  
+        IncludesActivities()
       )}
     </>
   );
