c1352fb685c85187211fed99124f253c7901e736
Tissyane <tissyane@gmail.com>
2023-01-11 18:34:12 -0300

    fix: activities - getting correct start and end time

--- src/components/Activities/ActivitiesCards.js
+++ src/components/Activities/ActivitiesCards.js
@@ -1,8 +1,11 @@
+import dayjs from 'dayjs';
 import styled from 'styled-components';
 
 export default function ActivitiesCards({ activitie }) {
-  const start = new Date(activitie.startsAt);
-  const end = new Date(activitie.endsAt);
+  const startsAt = dayjs(activitie.startsAt).add(3, 'h');
+  const endsAt = dayjs(activitie.endsAt).add(3, 'h');
+  const start = new Date(startsAt);
+  const end = new Date(endsAt);
   const duration = (end - start)/(3600000);
   const startTime = start.toTimeString().slice(0, 5);
   const endTime = end.toTimeString().slice(0, 5);
@@ -22,6 +25,7 @@ height: ${props => `${80 * props.duration}px`};
 background-color: #F1F1F1;
 border-radius: 5px;
 padding: 12px 10px;
+margin-bottom: 10px;
 `;
 
 const ActivitieTitle = styled.h4`
